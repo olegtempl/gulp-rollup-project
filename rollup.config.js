3ef8e2a73efd142e67f930c646ff27be0d64c984
olegtempl <linner_952@mail.ru>
2018-04-28 14:53:06 +0300

    add rollup config & change path

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+const path = require('./configs/path.json');
+
+export default {
+  input: path.src.js + path.src.fileJs,
+  output: {
+    file: path.build.js + path.src.fileJs,
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
+};
