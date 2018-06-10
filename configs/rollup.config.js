8953e662f6bb6e32bb52179b5540f4af02e9b26e
olegtempl <linner_952@mail.ru>
2018-06-11 01:33:44 +0200

    update config

--- configs/rollup.config.js
+++ configs/rollup.config.js
@@ -1,11 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+
 const path = require('./path.json');
 
 export default {
   input: path.src.js + path.src.startFileJs,
   output: {
-    file: path.build.js + path.src.endFileJs,
+    file: path.build.js + path.build.endFileJs,
     format: 'cjs'
   },
   plugins: [
